1c5bb5de75c41c7668fed76857fc1b6739636998
GarutCodeCamp <akuncode391@gmail.com>
2021-07-06 13:01:41 +0700

    Changed SuccessFully

---
+++ app.js
@@ -0,0 +1,9 @@
+function send(){
+    const input = document.getElementsByTagName("input")[0].value;
+    const input2 = document.getElementsByTagName("textarea")[0].value;
+
+    if(input != "" )alert("isi dari title:  "+ input)
+
+    if(input2 != "")alert("Isi Descriptsi:  "+input2)
+
+}
